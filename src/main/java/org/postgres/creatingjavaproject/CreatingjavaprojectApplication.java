2dfe25b330f195a2f00d454d110e9ef963d8980c
NAME <mail@gmail.com>
2025-01-06 22:06:22 +0300

    Add existing project files to Git

---
+++ src/main/java/org/postgres/creatingjavaproject/CreatingjavaprojectApplication.java
@@ -0,0 +1,13 @@
+package org.postgres.creatingjavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingjavaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingjavaprojectApplication.class, args);
+	}
+
+}
